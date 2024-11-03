37d3e4640513d5cf0c3524756430432f78f1c0da
wingie125 <arbab.vns123@gmail.com>
2024-11-03 14:23:25 +0530

    final

--- server.js
+++ server.js
@@ -9,54 +9,65 @@ const axios = require("axios");
 const OpenAI = require("openai");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 dotenv.config();
+const http = require("http");
+const server = http.createServer(app);
 const openai = new OpenAI({
   apiKey: process.env.OPEN_AI_KEY,
 });
-app.use(cors());
-const http = require("http");
-const server = http.createServer(app);
 
 const s3 = new S3Client({
   credentials: {
-    accessKeyId: process.env.ACCESS_KEY,
-    secretAccessKey: process.env.SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  region: process.env.BUCKET_REGION,
+  region: process.env.AWS_REGION,
 });
+
+app.use(cors());
+
 const io = new Server(server, {
   cors: {
     origin: process.env.ELECTRON_HOST,
     methods: ["GET", "POST"],
   },
 });
+
 let recordedChunks = [];
+
 io.on("connection", (socket) => {
-  console.log(`🟢 User connected`);
+  console.log("🚀 New client connected");
+
   socket.on("video-chunks", async (data) => {
-    console.log(`🟢 Video chunks received`);
+    console.log("📦 Received video chunks", data);
     const writeStream = fs.createWriteStream("temp_upload/" + data.filename);
     recordedChunks.push(data.chunks);
     const videoBlob = new Blob(recordedChunks, {
       type: "video/webm; codecs=vp9",
     });
     const buffer = Buffer.from(await videoBlob.arrayBuffer());
-    const readableStream = Readable.from(buffer);
-    readableStream.pipe(writeStream).on("finish", () => {
-      console.log(`🟢 Video saved to temp_upload`);
+
+    const readStream = Readable.from(buffer);
+    readStream.pipe(writeStream).on("finish", () => {
+      console.log("📦 Finished writing file");
     });
   });
   socket.on("process-video", async (data) => {
-    console.log(`🟢 Video processing requested`);
+    console.log("📽️ Processing video", data);
+    // ffmpeg -i input.webm -c copy -map 0 -movflags +faststart output.webm
+    // This command will copy the video and audio streams from the input.webm file to the output.webm file without re-encoding them and adding metadata at the beginning of the file.
     recordedChunks = [];
-    fs.readFile("temp_upload/" + data.fileName, async (err, file) => {
+    fs.readFile("temp_upload/" + data.filename, async (err, file) => {
       const processing = await axios.post(
         `${process.env.NEXT_API_HOST}recording/${data.userId}/processing`,
-        { filename: data.filename }
+        {
+          filename: data.filename,
+        }
       );
-      if (processing.data.status !== 200)
-        return console.log(`Error occurred while processing video`);
+      if (processing.data.status !== 200) {
+        return console.log("❌ Error processing video");
+      }
       const Key = data.filename;
-      const Bucket = process.env.BUCKET_NAME;
+      const Bucket = process.env.AWS_BUCKET_NAME;
       const ContentType = "video/webm";
       const command = new PutObjectCommand({
         Key,
@@ -65,12 +76,14 @@ io.on("connection", (socket) => {
         Body: file,
       });
       const fileStatus = await s3.send(command);
-      if (fileStatus["$metadata"].httpStatusCode === 200) {
-        console.log(`🟢 Video uploaded to S3`);
+
+      if (fileStatus.$metadata.httpStatusCode === 200) {
+        console.log("✅ File uploaded to S3");
+
         if (processing.data.plan === "PRO") {
-          fs.stat("temp_upload/" + data.filename, async (err, stat) => {
+          fs.stat("temp_upload/" + data.filename, async (err, stats) => {
             if (!err) {
-              if (stat.size < 25000000) {
+              if (stats.size < 25000000) {
                 const transcription = await openai.audio.transcriptions.create({
                   file: fs.createReadStream(`temp_upload/${data.filename}`),
                   model: "whisper-1",
@@ -79,13 +92,13 @@ io.on("connection", (socket) => {
                 if (transcription) {
                   const completion = await openai.chat.completions.create({
                     model: "gpt-3.5-turbo",
-                    response_format: { type: "json_object" },
+                    response_format: {
+                      type: "json_object",
+                    },
                     messages: [
                       {
                         role: "system",
-                        content: `You are a going to generate title and a nice description using the speech
-                          to text transcription provided: transcription(${transcription}) and then return
-                          it in  json format as {"title": <the title you gave>, "summary": <the summary you created>}`,
+                        content: `You are going to generate a title and a nice description using the speech to text transcription provided: transcription(${transcription}) and then return it in json format as {"title": <the title you gave>, "summary": <the summary you created>}`,
                       },
                     ],
                   });
@@ -97,40 +110,45 @@ io.on("connection", (socket) => {
                       transcript: transcription,
                     }
                   );
-                  if (titleAndSummaryGenerated.data.status !== 200)
-                    return console.log(
-                      `Error occurred while generating title and summary`
-                    );
+                  if (titleAndSummaryGenerated.data.status !== 200) {
+                    return console.log("❌ Error generating title and summary");
+                  }
                 }
               }
             }
           });
         }
+
         const stopProcessing = await axios.post(
           `${process.env.NEXT_API_HOST}recording/${data.userId}/complete`,
-          { filename: data.filename }
+          {
+            filename: data.filename,
+          }
         );
-        if (stopProcessing.data.status !== 200)
-          console.log(`Error occurred while stopping video processing`);
+        if (stopProcessing.data.status !== 200) {
+          return console.log("❌ Error stopping processing");
+        }
 
-        if (stopProcessing.status === 200) {
-          fs.unlink(`temp_upload/` + data.filename, (err) => {
-            if (!err)
-              console.log(
-                data.filename + " " + `🟢 Video deleted from temp_upload`
-              );
+        if (stopProcessing.data.status === 200) {
+          fs.unlink(`temp_upload/${data.filename}`, (err) => {
+            if (err) {
+              console.error("❌ Error deleting file", err);
+              return;
+            }
+            console.log(`✅ File deleted: ${data.filename}`);
           });
         }
       } else {
-        console.log(`Error occurred while deleting video from temp_upload`);
+        console.log("❌ Error uploading to S3");
       }
     });
   });
+
   socket.on("disconnect", () => {
-    console.log(`🟢 User disconnected`, socket.id);
+    console.log("🚫 Client disconnected");
   });
 });
 
 server.listen(5000, () => {
-  console.log("🟢 Opal Express is running on port 5000");
+  console.log("✅ Server is running on port 5000");
 });
